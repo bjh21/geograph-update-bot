c4497aaa56526bbbc65fc92b6995ed7f0a78f525
Ben Harris <bjh21@bjh21.me.uk>
2017-08-19 17:38:26 +0100

    A day's worth of hacking.  Uploads a single image for now.

---
+++ scripts/upgrade_size.py
@@ -0,0 +1,108 @@
+from __future__ import print_function
+
+import hashlib
+import logging
+import pywikibot
+import re
+import requests
+import sqlite3
+import tempfile
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+site = pywikibot.Site()
+geograph = pywikibot.Page(site, "Template:Geograph")
+
+geodb = sqlite3.connect('../geograph-db/geograph.sqlite3')
+
+client = requests.Session()
+client.headers['User-Agent'] = "upgrade_size (bjh21@bjh21.me.uk)"
+
+def get_geograph_info(gridimage_id):
+    # Use the oEmbed API
+    r = client.get("http://api.geograph.org.uk/api/oembed",
+                     params={'url': 'http://www.geograph.org.uk/photo/%d' %
+                                    gridimage_id,
+                             'format': 'json'})
+    r.raise_for_status()
+    j = r.json()
+    return j
+
+def get_geograph_basic(gridimage_id):
+    info = get_geograph_info(gridimage_id)
+    r = client.get(info['url'])
+    r.raise_for_status()
+    return r.content
+
+class StrangeURL(Exception):
+    pass
+
+def get_geograph_full(gridimage_id):
+    info = get_geograph_info(gridimage_id)
+    # Evil hack, but better than digging it out of HTML.
+    m = re.search(r"_([0-9a-f]{8})\.jpg$", info['url'])
+    if not m:
+        raise StrangeURL(info['url'])
+    imgkey = m.groups(1)
+    r = client.get("http://www.geograph.org.uk/reuse.php",
+                     params={'id': gridimage_id, 'download': imgkey,
+                             'size': 'original'})
+    r.raise_for_status()
+    return r.content
+
+def process_page(title):
+    page = pywikibot.FilePage(site, title)
+    templates = page.templatesWithParams()
+    geograph_templates = [t for t in templates if t[0] == geograph]
+    if len(geograph_templates) == 0:
+        logger.info("%s: no {{Geograph}} template", page)
+        return
+    if len(geograph_templates) > 1:
+        logger.warning("%s: %d {{Geograph}} templates", page)
+        return
+    try:
+        gridimage_id = int(geograph_templates[0][1][0])
+    except ValueError, IndexError:
+        logger.warning("%s: broken {{Geograph}} template", page)
+        return
+    logger.info("%s: Geograph ID is %d", page, gridimage_id)
+    c = geodb.cursor()
+    c.execute("""
+        SELECT width, height, original_width, original_height
+           FROM gridimage_size
+           WHERE gridimage_id = ?
+        """, (gridimage_id,))
+    if c.rowcount == 0:
+        logger.warning("%s: Geograph ID %d not in database",
+                       page, gridimage_id)
+        return
+    if c.rowcount > 1:
+        logger.error("Multiple database entries for Geograph ID %d",
+                     gridimage_id)
+        return
+    gwidth, gheight, original_width, original_height = c.fetchone()
+    if original_width == 0:
+        logger.info("%s: No high-res version available", page)
+        return
+    logger.info("%s: %dx%d version available",
+                page, original_width, original_height)
+    fi = page.latest_file_info
+    logger.info("%s: current Commons version is %dx%d",
+                page, fi.width, fi.height)
+    if (fi.width, fi.height) != (gwidth, gheight):
+        logger.info("%s: dimensions do not match Geograph basic image", page)
+        return
+    gb = get_geograph_basic(gridimage_id)
+    if hashlib.sha1(gb).hexdigest() != fi.sha1:
+        logger.info("%s: SHA-1 does not match Geograph basic image")
+    logger.info("%s: Image matches. Update possible.", page)
+    newimg = get_geograph_full(gridimage_id)
+    logger.info("Got %d bytes of image", len(newimg))
+    tf = tempfile.NamedTemporaryFile()
+    tf.write(newimg)
+    logger.info("File written to %s", tf.name)
+    page.upload(tf.name, comment="Higher-resolution version from Geograph",
+                ignore_warnings=['exists'])
+
+process_page('File:Manchester Town Hall October 2010.jpg')
