fe0ad3c0f75075e323d4ef441bc36513d4d9afab
Ben Harris <bjh21@bjh21.me.uk>
2017-10-02 23:18:55 +0100

    More tests and note IDs of test images.

--- scripts/location_test.py
+++ scripts/location_test.py
@@ -1,3 +1,5 @@
+from __future__ import division, print_function, unicode_literals
+
 import unittest
 from location import bng, ig, location_from_grid, location_from_row, en_from_gr
 
@@ -15,16 +17,26 @@ class GridLetterTests(unittest.TestCase):
         
 class FromRowTests(unittest.TestCase):
     def setUp(self):
-        self.full_row = dict(
+        self.full_row = dict(gridimage_id=4,
             grid_reference='SO8001', reference_index=1,
             nateastings=380930, natnorthings=201360, natgrlen='8',
             viewpoint_eastings=380980, viewpoint_northings=201340,
             viewpoint_grlen='8', view_direction=292, use6fig=1)
-        self.min_row = dict(
+        self.min_row = dict(gridimage_id=5,
             grid_reference='SO8201', reference_index=1,
             nateastings=0, natnorthings=0, natgrlen='4',
             viewpoint_eastings=0, viewpoint_northings=0,
             viewpoint_grlen='0', view_direction=-1, use6fig=0)
+        self.mid_row = dict(gridimage_id=2913,
+            grid_reference='W2076', reference_index=2,
+            nateastings=120800, natnorthings=76500, natgrlen='6',
+            viewpoint_eastings=0, viewpoint_northings=0,
+            viewpoint_grlen='0', view_direction=-1, use6fig=0)
+        self.high_row = dict(gridimage_id=715,
+            grid_reference='SO8474', reference_index=1,
+            nateastings=0, natnorthings=0, natgrlen='4',
+            viewpoint_eastings=384732, viewpoint_northings=274929,
+            viewpoint_grlen='10', view_direction=-1, use6fig=0)
     def test_full_row(self):
         s = str(location_from_row(self.full_row))
         self.assertEqual(s,
@@ -32,7 +44,15 @@ class FromRowTests(unittest.TestCase):
     def test_minimal_row(self):
         s = str(location_from_row(self.min_row))
         self.assertEqual(s,
-            "{{Location|51.711956|-2.254684|source:geograph|prec=700}}")
+            "{{Object location|51.712|-2.25|source:geograph|prec=700}}")
+    def test_medium_row(self):
+        s = str(location_from_row(self.mid_row))
+        self.assertEqual(s,
+            "{{Object location|51.9360|-9.152|source:geograph|prec=70}}")
+    def test_high_row(self):
+        s = str(location_from_row(self.high_row))
+        self.assertEqual(s,
+            "{{Location|52.372194|-2.22568|source:geograph|prec=0.7}}")
         
 if __name__ == '__main__':
     unittest.main()
