60809bde4ec29e276f4d085df8408ca1139674a0
Ben Harris <bjh21@bjh21.me.uk>
2018-01-27 18:43:01 +0000

    creditline: More thorough wikification of strings from database.

--- scripts/creditline.py
+++ scripts/creditline.py
@@ -5,13 +5,19 @@ from mwparserfromhell import parse
 from mwparserfromhell.nodes import Tag, Template, Text
 from mwparserfromhell.wikicode import Wikicode
 
+import re
+
+def wikify(x):
+    # Convert a string from the Geograph database into Wikicode.
+    x = re.sub(r"&#(\d+);", lambda m: chr(int(m.group(1))), x)
+    if ("''" in x or '}}' in x or '|' in x or '[[' in x):
+        return Tag('nowiki', Text(x))
+    return Text(x)
+
 def creditline_from_row(row):
-    t = Template(parse('Credit line'))
-    t.add("Author", Text(row['realname']))
-    title = Text(row['title'])
-    if ("''" in title or '}}' in title or '|' in title or '[[' in title):
-        title = Tag('nowiki', title)
-    t.add("Other", Tag('i', title, wiki_markup="''"))
+    t = Template(parse('Credit line\n '))
+    t.add("Author", wikify(row['realname']))
+    t.add("Other", Tag('i', wikify(row['title']), wiki_markup="''"))
     t.add("License",
         parse('[https://creativecommons.org/licenses/by-sa/2.0/ CC BY-SA 2.0]'))
     return t
